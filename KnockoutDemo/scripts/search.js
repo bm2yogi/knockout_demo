8fa8e9dcd780a98a42a881a09aea382c8319e823
Michael Ibarra <AMER\mibarra@gettyimages.com>
2011-12-16 17:12:40 -0800

    First pass observableArray examples.

---
+++ KnockoutDemo/scripts/search.js
@@ -0,0 +1,17 @@
+﻿var viewModel = {
+    query: ko.observable('water'),
+
+    search: function () {
+        $.getJSON('scripts/searchresults.js', this.showResults);
+    },
+
+    searchResults: ko.observableArray([]),
+
+    showResults: function (data) {
+        this.searchResults = ko.observableArray(data.Assets);
+    }
+};
+
+$(document).ready(function () {
+    ko.applyBindings(viewModel); 
+});
